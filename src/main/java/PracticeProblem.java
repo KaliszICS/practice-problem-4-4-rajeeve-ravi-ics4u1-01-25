5611833a19a91d9e30d4f8d5efa4c30767d02789
rajeeve-ravi-ics4u1-01-25 <rajeeve.ravi@student.tdsb.on.ca>
2026-01-20 13:17:29 -0500

    Update PracticeProblem.java
    done done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -10,42 +10,6 @@ public class PracticeProblem {
      */
     public static void main(String args[]) {
     }
-
-    /**
-     * Q 1.
-     */
-    public static void q1() {
-        //Write question 1 code here
-    }
-
-    /**
-     * Q 2.
-     */
-    public static void q2() {
-        //Write question 2 code here
-    }
-
-    /**
-     * Q 3.
-     */
-    public static void q3() {
-        //Write question 3 code here
-    }
-
-    /**
-     * Q 4.
-     */
-    public static void q4() {
-        //Write question 4 code here
-    }
-
-    /**
-     * Q 5.
-     */
-    public static void q5() {
-        //Write question 5 code here
-    }
-
 }
 
 /**
